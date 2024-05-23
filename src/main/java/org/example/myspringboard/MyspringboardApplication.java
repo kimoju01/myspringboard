7a98d2d22b052b61c82fc72d9629545b21aac048
Hyeju Kim <kimoju03@naver.com>
2024-05-23 17:50:28 +0900

    Initial commit

---
+++ src/main/java/org/example/myspringboard/MyspringboardApplication.java
@@ -0,0 +1,13 @@
+package org.example.myspringboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyspringboardApplication.class, args);
+    }
+
+}
