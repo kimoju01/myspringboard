e3abffac3e751a7731a3c9a8dc2e96b74eba5a0e
Hyeju Kim <kimoju03@naver.com>
2024-05-24 10:46:12 +0900

    feat: add 글 등록

--- src/main/java/org/example/myspringboard/controller/BoardController.java
+++ src/main/java/org/example/myspringboard/controller/BoardController.java
@@ -1,11 +1,15 @@
 package org.example.myspringboard.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.myspringboard.domain.Board;
 import org.example.myspringboard.service.BoardService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequiredArgsConstructor
@@ -31,6 +35,18 @@ public class BoardController {
     }
 
     // 글 등록
+    @GetMapping("/writeform")
+    public String writeForm(Model model) {
+        model.addAttribute("board", new Board());
+        return "board/writeForm";
+    }
+
+    @PostMapping("/write")
+    public String write(@ModelAttribute Board board, RedirectAttributes redirectAttributes) {
+        boardService.saveBoard(board);
+        redirectAttributes.addFlashAttribute("message", "게시글이 정상적으로 등록되었습니다.");
+        return "redirect:/list";
+    }
 
 
     // 글 수정
